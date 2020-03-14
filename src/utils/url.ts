653f044d60f8d844525ca7c46d8d2c3dd1032e6e
Anton Smirnov <smiranton.kiev@gmail.com>
2020-03-14 10:16:57 +0200

    Refactored

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -8,19 +8,10 @@ const bindProtocol = (url: string) => {
   return url.includes('http') ? url : `${window.location.protocol}//${url}`;
 };
 
-/**
- * Coerce http/https protocol
- * @param  data Data to parse
- * @return Coerced data
- */
 export const coerceProtocol = (data: string | string[]): string | string[] => {
   return isArray(data) ? data.map(bindProtocol) : bindProtocol(data);
 };
 
-/**
- * Define base server url
- * @return Server base url
- */
 export const getBaseUrl = (): string => {
   const baseUrl = process.env.REACT_APP_BASE_API_URL || 'missingBaseApiUrlEnvVar';
   return last(baseUrl) !== '/' ? `${baseUrl}/` : baseUrl;
